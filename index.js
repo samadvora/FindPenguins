5b134d4bf8f420a8e7c5778a6f83278a18f3454b
Samad Vora <samadvora166@gmail.com>
2025-08-03 13:56:50 -0400

    index.js

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 $(document).ready(function () {
-    var c = 0;
-    var h = 0;
+    var c = 0; // Current score
+    var h = 0; // High score
+    var timeLeft = 30; // Timer set to 30 seconds
+    var timer; // Variable to hold the timer interval
+
     $("#score").html('Score : ' + c + '<br> High Score : ' + h);
-    
+    $("#timer").hide(); // Hide timer initially
+
     $("#startButton").click(function() {
         RandomImage();
         $("#startButton").hide(); // Hide the start button after starting the game
+        $("#timer").show(); // Show the timer
+        startTimer(); // Start the timer
     });
 
     $(".Penguins").on('click', function () {
-        c++;
-        var num = $(this).attr('id');
-        var Char = num.substr(num.length - 1);
-        
-        if ($(this).hasClass("yeti")) {
-            $(this).css('background-image', 'url(images/yeti.png)');
-            alert("Game Over! It's a Yeti!");
-            h = c;
-            $("#score").html('Score: ' + c + '<br> High Score : ' + h);
-            location.reload();
-        } else {
-            $(this).css('background-image', 'url(images/penguin_' + Char + '.png)');
-            $("#score").html('Score : ' + c + '<br> High Score : ' + h);
-        }
-        
-        if (c == 8) {
-            alert("Congratulations! You have won the game!");
-            location.reload();
+        if (timeLeft > 0) { // Only allow clicks if time is left
+            c++;
+            var num = $(this).attr('id');
+            var Char = num.substr(num.length - 1);
+            
+            if ($(this).hasClass("yeti")) {
+                $(this).css('background-image', 'url(images/yeti.png)');
+                alert("Game Over! It's a Yeti!");
+                h = c;
+                $("#score").html('Score: ' + c + '<br> High Score : ' + h);
+                resetGame();
+            } else {
+                $(this).css('background-image', 'url(images/penguin_' + Char + '.png)');
+                $("#score").html('Score : ' + c + '<br> High Score : ' + h);
+            }
+            
+            if (c == 8) {
+                alert("Congratulations! You have won the game!");
+                resetGame();
+            }
         }
     });
 
@@ -34,5 +42,29 @@ $(document).ready(function () {
         var n = Math.floor(Math.random() * 9);
         $("#penguin" + n).addClass('yeti');
         $("#penguin" + n).attr('id', 'yeti');
+    }
+
+    function startTimer() {
+        timer = setInterval(function() {
+            timeLeft--;
+            $("#time").text(timeLeft); // Update timer display
+            if (timeLeft <= 0) {
+                clearInterval(timer);
+                alert("Time's up! Your score is: " + c);
+                resetGame();
+            }
+        }, 1000); // Update every second
+    }
+
+    function resetGame() {
+        clearInterval(timer);
+        timeLeft = 30; // Reset timer
+        c = 0; // Reset score
+        $("#score").html('Score : ' + c + '<br> High Score : ' + h);
+        $("#time").text(timeLeft); // Reset timer display
+        $(".Penguins").css('background-image', ''); // Clear images
+        $(".Penguins").removeClass('yeti'); // Remove yeti class
+        $("#startButton").show(); // Show start button again
+        $("#timer").hide(); // Hide timer
     }
 });
