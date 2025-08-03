e22398d5d1d66acedbfbc7e4dca7837f121082ab
Samad Vora <samadvora166@gmail.com>
2025-08-03 13:43:06 -0400

    index.js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 $(document).ready(function () {
-var c = 0;
-var h = 0;
-$("#score").html('Score : '+c+'<br> High Score : '+h);
-$("#yeti").mousedown(function () {
-alert("Yaaaarrrr!");
-} );
-RandomImage();
-
-$(".Penguins").on('click', function () {  
- c = c + 1;
-var num = $(this).attr('id');
-var Char = num.substr(num.length - 1);
-if ($(this).hasClass("Penguins yeti"))
-{  
-$(this).css('background-image', 'url(images/yeti.png)');
-alert("GameOver ! Its Yetiiit");
-h = c;
-$("#score").html('Score: '+c+'<br> High Score : '+ h);
-location.reload();
-}
-else
-{
-$(this).css('background-image', 'url(images/penguin_' + Char + '.png)');  
-$("#score").html('Score : '+c+'<br> High Score : '+h);
+    var c = 0;
+    var h = 0;
+    $("#score").html('Score : ' + c + '<br> High Score : ' + h);
     
-}
-if(c==8)
-{            
-alert("congratulation you have won the game");
-}
-});
-function RandomImage() {
- var n = Math.floor(Math.random() * 9);
-$("#penguin" + n).addClass('yeti');
-$("#penguin" + n).attr('id', 'yeti');   
-}
+    $("#startButton").click(function() {
+        RandomImage();
+        $("#startButton").hide(); // Hide the start button after starting the game
+    });
+
+    $(".Penguins").on('click', function () {
+        c++;
+        var num = $(this).attr('id');
+        var Char = num.substr(num.length - 1);
+        
+        if ($(this).hasClass("yeti")) {
+            $(this).css('background-image', 'url(images/yeti.png)');
+            alert("Game Over! It's a Yeti!");
+            h = c;
+            $("#score").html('Score: ' + c + '<br> High Score : ' + h);
+            location.reload();
+        } else {
+            $(this).css('background-image', 'url(images/penguin_' + Char + '.png)');
+            $("#score").html('Score : ' + c + '<br> High Score : ' + h);
+        }
+        
+        if (c == 8) {
+            alert("Congratulations! You have won the game!");
+            location.reload();
+        }
+    });
+
+    function RandomImage() {
+        var n = Math.floor(Math.random() * 9);
+        $("#penguin" + n).addClass('yeti');
+        $("#penguin" + n).attr('id', 'yeti');
+    }
 });
